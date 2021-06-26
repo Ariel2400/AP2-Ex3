3d384332d62bea0d9da3821b74b3f8211b2d0bc0
ariel <ariel@DESKTOP-TBI0PE8>
2021-06-26 18:26:04 +0300

    finished the ViewModel including data binding

--- app/src/main/java/com/example/ap2_ex3/viewModel/ViewModel.java
+++ app/src/main/java/com/example/ap2_ex3/viewModel/ViewModel.java
@@ -1,8 +1,67 @@
 package com.example.ap2_ex3.viewModel;
 
+import com.example.ap2_ex3.model.Model;
+
 public class ViewModel {
-    String ip;
-    String port;
-    int aileron, throttle;
+    public String ip;
+    public String port;
+    public int rudder, throttle;
+    public float aileron, elevator;
+    Model model;
+    public ViewModel(){
+        model = new Model(this);
+    }
+    public String getIp() {
+        return ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public float getRudder() {
+        return rudder;
+    }
+
+    public float getThrottle() {
+        return throttle;
+    }
+
+    public float getAileron() {
+        return aileron;
+    }
+
+    public float getElevator() {
+        return elevator;
+    }
+
+    public void connect(){
+        model.connect();
+    }
+
+    public void setThrottle(){
+        try{
+            model.setThrottle();
+        } catch (InterruptedException e){}
+    }
+
+    public void setAileron(){
+        try{
+            model.setAileron();
+        } catch (InterruptedException e){}
+    }
+
+    public void setRudder(){
+        try{
+            model.setRudder();
+        } catch (InterruptedException e){}
+    }
+
+    public void setElevator(){
+        try{
+            model.setThrottle();
+        } catch (InterruptedException e){}
+    }
+
 
 }
