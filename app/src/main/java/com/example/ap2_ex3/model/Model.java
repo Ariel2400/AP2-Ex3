3daa509bc84c64d1bd5b6e83c1b3283077ce0184
ariel <ariel@DESKTOP-TBI0PE8>
2021-06-26 18:27:20 +0300

    finished the Model

--- app/src/main/java/com/example/ap2_ex3/model/Model.java
+++ app/src/main/java/com/example/ap2_ex3/model/Model.java
@@ -2,17 +2,55 @@ package com.example.ap2_ex3.model;
 
 import com.example.ap2_ex3.viewModel.ViewModel;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Model {
     ViewModel mViewModel;
+    PrintWriter out;
     Socket socket;
+    BlockingQueue<Runnable> dispatchQueue = new LinkedBlockingQueue<>();
+
 
     public Model(ViewModel viewModel){
         mViewModel = viewModel;
     }
 
-    public void connect(){
-        socket = new Socket()
+    public void connect()  {
+        try{
+            socket = new Socket(mViewModel.getIp(),Integer.parseInt(mViewModel.getPort()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+            new Thread(() -> {
+                while(true){
+                    try{
+                        dispatchQueue.take().run();
+                    }catch (InterruptedException e){}
+                }
+            }).start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setThrottle() throws InterruptedException {
+        dispatchQueue.put(() -> out.print("set /controls/engines/current-engine/throttle" + mViewModel.getThrottle()));
+    }
+
+    public void setAileron() throws InterruptedException{
+        dispatchQueue.put(() -> out.print("set /controls/flight/aileron" + mViewModel.getAileron()));
+    }
+
+    public void setElevator() throws InterruptedException{
+        dispatchQueue.put(() -> out.print("set /controls/flight/elevator" + mViewModel.getElevator()));
+    }
+
+    public void setRudder() throws InterruptedException{
+        dispatchQueue.put(() -> out.print("set /controls/flight/rudder" + mViewModel.getRudder()));
     }
 }
